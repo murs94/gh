b3fcb024087543fd1a43e176ce2da9d13d26b1ab
mursyid.hazizan <mursyid.hazizan@pacificinternet.com>
2024-09-19 14:22:14 +0800

    fiirst commit

---
+++ app/Livewire/Reservation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Reservation extends Component
+{
+    public function render()
+    {
+        return <<<'HTML'
+        <div>
+            {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+        </div>
+        HTML;
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//login/logout routes
+Auth::routes();
+
+Route::middleware(['auth'])->group(function (){
+    Route::get('/', App\Livewire\Dashboard::class)->name(name: 'dashboard'); //home
+});
+
+//prefix -- to be use
+Route::prefix('reservation')->middleware(['auth'])->group(function () {
+    Route::get('/', App\Livewire\Dashboard::class)->name(name: 'dashboard');
+});
